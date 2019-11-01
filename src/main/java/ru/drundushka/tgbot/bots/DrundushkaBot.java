4d8602a9fcb8784378b54e1cc153c13dd580ff5a
Verkhoturkin <verhoturkin@gmail.com>
2019-11-01 13:55:52 +0500

    Initial commit

---
+++ src/main/java/ru/drundushka/tgbot/bots/DrundushkaBot.java
@@ -0,0 +1,54 @@
+package ru.drundushka.tgbot.bots;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.List;
+
+@Component
+public class DrundushkaBot extends TelegramLongPollingBot {
+    @Autowired
+    ApplicationArguments args;
+
+    private List<String> nonOptionArgs = args.getNonOptionArgs();
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
+            if (message.hasText()) {
+                String text = message.getText();
+                try {
+                    execute(sendMsg(update.getMessage().getChatId().toString(), text));
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private synchronized SendMessage sendMsg(String chatId, String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        return sendMessage;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return nonOptionArgs.get(0);
+    }
+
+    @Override
+    public String getBotToken() {
+        return nonOptionArgs.get(1);
+    }
+}
---
+++ src/test/java/ru/drundushka/tgbot/TgbotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.drundushka.tgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
